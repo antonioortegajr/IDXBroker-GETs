ae5eea2dad20d87bffd53ec4dc49bdb851c3296e
antonioortegajr <antonioortegajr@gmail.com>
2015-02-17 19:39:52 -0800

    Update GetLeads.py
    added notes

--- GetLeads.py
+++ GetLeads.py
@@ -1,6 +1,8 @@
 import httplib
 import urlparse
 
+#this is a sample call to base your API calls off of.
+
 url = "https://api.idxbroker.com/leads/lead"
 headers = {"Content-Type":"application/x-www-form-urlencoded", "accesskey":"YourAPIKeyHere"}
 
@@ -8,7 +10,10 @@ domain = urlparse.urlparse(url).netloc
 connection = httplib.HTTPSConnection(domain)
 connection.request("GET", url, headers=headers)
 response = connection.getresponse()
-
+# print the http status code returned. Expecting 200
 print "status: " + str(response.status), response.reason
+#print the response headers
 print response.getheaders()
+#print the response body
 print response.read()
+
