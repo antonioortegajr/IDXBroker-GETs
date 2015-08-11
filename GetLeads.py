e00da9cb276feb7190d2a4e5f07b46d381a07ed1
antonioortegajr <antonioortegajr@gmail.com>
2015-08-11 14:02:19 -0700

    Update GetLeads.py
    Changed to function for easier testing in python Interpreter

--- GetLeads.py
+++ GetLeads.py
@@ -2,18 +2,20 @@ import httplib
 import urlparse
 
 #this is a sample call to base your API calls off of.
+def main(url, headers):
+  domain = urlparse.urlparse(url).netloc
+  connection = httplib.HTTPSConnection(domain)
+  connection.request("GET", url, headers=headers)
+  response = connection.getresponse()
+  # print the http status code returned. Expecting 200
+  print "status: " + str(response.status), response.reason
+  #print the response headers
+  print response.getheaders()
+  #print the response body
+  print response.read()
 
+#set endpoint and add your API key
 url = "https://api.idxbroker.com/leads/lead"
 headers = {"Content-Type":"application/x-www-form-urlencoded", "accesskey":"YourAPIKeyHere"}
-
-domain = urlparse.urlparse(url).netloc
-connection = httplib.HTTPSConnection(domain)
-connection.request("GET", url, headers=headers)
-response = connection.getresponse()
-# print the http status code returned. Expecting 200
-print "status: " + str(response.status), response.reason
-#print the response headers
-print response.getheaders()
-#print the response body
-print response.read()
-
+#run function
+main(url, headers)
